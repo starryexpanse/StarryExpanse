30ee9e40ac52602bb122ce3aaa78bae581dabbdd
Philip Peterson <philip@amplitude.com>
2016-09-30 22:19:29 -0700

    uncomment HUD stuff

--- StarryExpanse/Base/GUI/SEHUD.cpp
+++ StarryExpanse/Base/GUI/SEHUD.cpp
@@ -18,7 +18,6 @@ void ASEHUD::DrawHUD()
 {
    Super::DrawHUD();
 
-   /*
    if (IsValid(CursorTexture.Texture))
    {
       // Find center of the Canvas
@@ -45,7 +44,6 @@ void ASEHUD::DrawHUD()
       TileItem.SetColor(FLinearColor::FromSRGBColor(FColor::White.WithAlpha(uint8(CursorOpacity * float(255)))));
       Canvas->DrawItem(TileItem);
    }
-   */
 }
 
 UTexture2D *ASEHUD::GetCursorTexture() {
