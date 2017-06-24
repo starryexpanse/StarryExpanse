163c8f5f2109feee72c8b75e0a0714da4b610a67
Chris Mumford <chris@starryexpanse.com>
2017-06-24 12:56:40 -0700

    Switched to #pragma once in PCH file.
    This is also the new boilerplate generated by UE4.16.

--- StarryExpanse/StarryExpanse.h
+++ StarryExpanse/StarryExpanse.h
@@ -1,11 +1,8 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
-#ifndef __STARRYEXPANSE_H__
-#define __STARRYEXPANSE_H__
+#pragma once
 
 #include "Engine.h"
 #include "Base/Globals/SEGlobalEventStates.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(StarryDebug, Log, All);
-
-#endif
