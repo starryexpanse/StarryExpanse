62df5b05d0f70bcfeae62f26382a758d2977b21a
Chris Mumford <chris@starryexpanse.com>
2017-06-24 13:10:28 -0700

    Switched from OutExtraModuleNames.Add tp ExtraModuleNames.Add.
    Old way was deprecated and produced a deprecated compiler warning after
    switching to UE4 v4.16.

--- StarryExpanse.Target.cs
+++ StarryExpanse.Target.cs
@@ -5,21 +5,9 @@ using System.Collections.Generic;
 
 public class StarryExpanseTarget : TargetRules
 {
-	public StarryExpanseTarget(TargetInfo Target)
+	public StarryExpanseTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.Add("StarryExpanse");
+		ExtraModuleNames.Add("StarryExpanse");
 	}
 }
