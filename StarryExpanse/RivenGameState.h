cee00e50ee0df05d0c8eb36cc71721f4d4450269
Philip Peterson <philip.c.peterson@gmail.com>
2018-01-01 17:02:41 -0500

    Attempt to fix circular dependency

--- StarryExpanse/RivenGameState.h
+++ StarryExpanse/RivenGameState.h
@@ -3,11 +3,12 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "GameFramework/GameStateBase.h"
-#include "SaveGame/RivenSaveGame.h"
 #include "Interfaces/RivenSavegameAware.h"
 #include "Runtime/Core/Public/UObject/WeakObjectPtrTemplates.h"
 #include "Runtime/Core/Public/GenericPlatform/GenericPlatform.h"
 #include "RivenGameState.generated.h"
+
+class URivenSaveGame;
 
 UCLASS()
 class ARivenGameState : public AGameStateBase {
