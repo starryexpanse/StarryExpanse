6063b418178924cdde06851101e638f34cb88f18
Philip Peterson <im@starryexpanse.com>
2019-06-27 23:03:44 -0700

    Clean up

--- StarryExpanse/Actors/MotionController/VRMotionController.cpp
+++ StarryExpanse/Actors/MotionController/VRMotionController.cpp
@@ -40,9 +40,6 @@ void AVRMotionController::BeginPlay() {
   if (GEngine)
     GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan,
                                      TEXT("Spawned MotionController"));
-
-  // Initial transform target is its own transform
-  m_RepTransformTarget = GetActorTransform();
 }
 
 // ===================================================
@@ -67,120 +64,45 @@ void AVRMotionController::RetrieveMotionControllerDevice() {
 void AVRMotionController::Tick(float DeltaTime) {
   Super::Tick(DeltaTime);
 
-  switch (m_TrackingMode) {
-
-    // ---- NONE ----
-  case ETrackingMode::None:
-    break;
-
-    // ---- TRACKED ----
-  case ETrackingMode::Tracked: {
-    // 1. Tracking can be (temporarily) disabled
-    if (m_bTrackingEnabled) {
-
-      // 2. Controller device is needed to track MoCon
-      if (!m_pController) {
-        // Try to retrieve before using motion controller device
-        RetrieveMotionControllerDevice();
-      }
-
-      if (m_pController) {
-        FVector trackedLoc;
-        FRotator trackedRot;
-        const float meterInUnrealUnits{100.f};
-
-        // Motion Source enum to FName
-        FName motionSourceName{};
-        if (m_ControllerHand == EControllerHand::Left) {
-          motionSourceName = "Left";
-        } else if (m_ControllerHand == EControllerHand::Right) {
-          motionSourceName = "Right";
-        } else {
-          motionSourceName = "Left";
-        }
-
-        // POLL LOCATION and ROTATION
-        m_pController->GetControllerOrientationAndPosition(
-            0, motionSourceName, trackedRot, trackedLoc, meterInUnrealUnits);
-
-        // SET polled LOCATION and ROTATION
-        if (GetAttachParentActor()) {
-          SetActorRelativeRotation(trackedRot);
-          SetActorRelativeLocation(trackedLoc);
-        } else {
-          SetActorLocationAndRotation(trackedLoc, trackedRot);
-        }
-
-        // Send relative transform
-        FTransform toBroadcast = GetActorTransform();
-        toBroadcast.SetLocation(trackedLoc);
-        toBroadcast.SetRotation(trackedRot.Quaternion());
-
-        Server_BroadcastTransform(toBroadcast);
-      }
+  if (m_bIsTracking) {
+    // 2. Controller device is needed to track MoCon
+    if (!m_pController) {
+      // Try to retrieve before using motion controller device
+      RetrieveMotionControllerDevice();
     }
 
-    break;
-  }
+    if (m_pController) {
+      FVector trackedLoc;
+      FRotator trackedRot;
+      const float meterInUnrealUnits{100.f};
 
-  // ---- REPLICATED ---- TODO REMOVE ME
-  case ETrackingMode::Replicated: {
-    // Debug show replicated target location and current location
-    if (false && m_ControllerHand == EControllerHand::Left) {
-      // Purple = Where actor currently is
-      // Green = Where actor is moving towards
-      DrawDebugSphere(GetWorld(), m_pHandOrigin->GetComponentLocation(), 15.f,
-                      32, FColor::Purple, false, -1.f,
-                      ESceneDepthPriorityGroup::SDPG_Foreground);
-
-      FVector targetLocation{};
-      USceneComponent *pParentCmp = m_pHandOrigin->GetAttachParent();
-      if (pParentCmp) {
-        targetLocation = {pParentCmp->GetComponentTransform().TransformPosition(
-            m_RepTransformTarget.GetLocation())};
+      // Motion Source enum to FName
+      FName motionSourceName{};
+      if (m_ControllerHand == EControllerHand::Left) {
+        motionSourceName = "Left";
+      } else if (m_ControllerHand == EControllerHand::Right) {
+        motionSourceName = "Right";
       } else {
-        targetLocation = m_RepTransformTarget.GetLocation();
+        motionSourceName = "Left";
       }
 
-      DrawDebugSphere(GetWorld(), targetLocation, 15.f, 32, FColor::Green);
+      // POLL LOCATION and ROTATION
+      m_pController->GetControllerOrientationAndPosition(
+          0, motionSourceName, trackedRot, trackedLoc, meterInUnrealUnits);
+
+      // SET polled LOCATION and ROTATION
+      if (GetAttachParentActor()) {
+        SetActorRelativeRotation(trackedRot);
+        SetActorRelativeLocation(trackedLoc);
+      } else {
+        SetActorLocationAndRotation(trackedLoc, trackedRot);
+      }
     }
-
-    // DoReplication();
-    // return;
-
-    // Not tracked, should lerp to replicated transform
-    const float lerpSpeed{10.f};
-    float alpha = FMath::Clamp<float>(DeltaTime * lerpSpeed, 0.f, 1.f);
-
-    USceneComponent *pAttachScnCmp{RootComponent->GetAttachParent()};
-    if (pAttachScnCmp) {
-      // FTransform originTransform = pAttachScnCmp->GetComponentTransform();
-      //
-      // FTransform currentRelativeTransform = originTransform.Inverse() *
-      // GetActorTransform();
-
-      FTransform currentTransform = GetActorTransform();
-      FTransform originTransform = pAttachScnCmp->GetComponentTransform();
-      FTransform targetTransform{m_RepTransformTarget * originTransform};
-
-      FVector lerpedLocation{FMath::Lerp(currentTransform.GetLocation(),
-                                         targetTransform.GetLocation(), alpha)};
-      FQuat lerpedRotation{FMath::Lerp(currentTransform.GetRotation(),
-                                       targetTransform.GetRotation(), alpha)};
-      FVector lerpedScale{FMath::Lerp(currentTransform.GetScale3D(),
-                                      targetTransform.GetScale3D(), alpha)};
-
-      SetActorTransform(FTransform(lerpedRotation, lerpedLocation, lerpedScale),
-                        false, nullptr, ETeleportType::ResetPhysics);
-    }
-
-    break;
-  }
   }
 }
 
-void AVRMotionController::SetTrackingMode(ETrackingMode trackingMode) {
-  m_TrackingMode = trackingMode;
+void AVRMotionController::SetIsTracking(bool isTracking) {
+  m_bIsTracking = isTracking;
 }
 
 void AVRMotionController::SetTrackingHand(EControllerHand hand) {
@@ -193,33 +115,4 @@ void AVRMotionController::SetTrackingHand(EControllerHand hand) {
                 m_pControllerMesh->RelativeScale3D.Y,
                 m_pControllerMesh->RelativeScale3D.Z * -1));
   }
-}
-
-void AVRMotionController::DoReplication_Implementation() {}
-
-#pragma region Replication
-// ===================================================
-void AVRMotionController::Server_BroadcastTransform_Implementation(
-    FTransform replicatedTransform) {
-  Multicast_ReplicateTransform(replicatedTransform);
-}
-
-bool AVRMotionController::Server_BroadcastTransform_Validate(
-    FTransform replicatedTransform) {
-  return true;
-}
-
-// ===================================================
-void AVRMotionController::Multicast_ReplicateTransform_Implementation(
-    FTransform replicatedTransform) {
-  // Only for non-owning clients/server
-  if (m_TrackingMode == ETrackingMode::Replicated) {
-    m_RepTransformTarget = replicatedTransform;
-  }
-}
-
-// ===================================================
-bool AVRMotionController::Multicast_ReplicateTransform_Validate(
-    FTransform replicatedTransform) {
-  return true;
 }
